7c557cc9379ee3613521555ecff7e0198bc615d5
Arkadiusz <108217975+ArkadiuszBorcz94@users.noreply.github.com>
2023-06-29 17:06:38 +0200

    Add files via upload

---
+++ Models/TimeModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WebTime.Models
+{
+    public class TimeModel
+    {
+
+        public int TimeId { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan BreakTime { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
